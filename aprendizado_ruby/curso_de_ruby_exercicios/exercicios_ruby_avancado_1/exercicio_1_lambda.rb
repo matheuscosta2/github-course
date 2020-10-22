a6767c54922329b3528cc88efaa5457c6ed4ee8c
Matheus Costa <matheuscostajgppo@gmail.com>
2020-10-22 20:52:58 -0300

    add exercise about lambda2

--- aprendizado_ruby/curso_de_ruby_exercicios/exercicios_ruby_avancado_1/exercicio_1_lambda.rb
+++ aprendizado_ruby/curso_de_ruby_exercicios/exercicios_ruby_avancado_1/exercicio_1_lambda.rb
@@ -1,8 +1,13 @@
-my_lambdaa = lambda (capitalize_name) do |lambda_1, lamba_2|
-    lambda_1.call
-    lambda_1.capitalize 
-    lambda_2.call
-    lambda_2.capitalize
-end 
+def capitalize_name (lambda_capitalize)
+    lambda_capitalize.call('matheus')
+    lambda_capitalize.call('ananda')
+end
+
+lambda_capitalize = -> (name) { puts name.capitalize }
+
+capitalize_name(lambda_capitalize)
+
+
+
 
 
